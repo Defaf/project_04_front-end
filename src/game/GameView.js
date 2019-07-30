ccafbbbd08b45bf23bb0a18af1ebfc7fa225f5a3
Defaf <defaf_kamfar@hotmail.com>
2019-07-30 12:32:55 +0300

    update component name

--- src/game/GameView.js
+++ src/game/GameView.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-// import $ from "jquery"
 import './GameView.css'
 import Ken from './Ken'
-import Ryu from './Ryu'
+import Dudley from './Dudley'
 
-let width = 1100;
-let height = 560
+let width = 1100
+let height = 540
 const ratio = window.devicePixelRatio || 1;
 // window.innerHeight;
 
@@ -17,6 +16,7 @@ class GameView extends Component {
         this.canvas = React.createRef()
         this.player1 = React.createRef()
         this.player2 = React.createRef()
+        this.progress = React.createRef()
     }
 
     state = {
@@ -25,66 +25,120 @@ class GameView extends Component {
             height: height,
             ratio: ratio
         },
-        player1Health : 50,
-        player2Health : 50
+        playersDist : 300,
+        player1Health : 300,
+        player2Health : 300,
+        player1Win : 0, 
+        player2Win: 0 
     };
     
-    checkCollision = () => {
+    checkCollisionP1 = () => {
         console.log(ReactDOM.findDOMNode(this.player1.current).childNodes[0].getBoundingClientRect().left + "p1")
         console.log(ReactDOM.findDOMNode(this.player2.current).childNodes[0].getBoundingClientRect().left + "p2")
 
-        if(ReactDOM.findDOMNode(this.player1.current).childNodes[0].getBoundingClientRect().left > ReactDOM.findDOMNode(this.player2.current).childNodes[0].getBoundingClientRect().left ){
-            console.log('collision happend !!')
-            return true
-        }else if (ReactDOM.findDOMNode(this.player1.current).childNodes[0].getBoundingClientRect().left == ReactDOM.findDOMNode(this.player2.current).childNodes[0].getBoundingClientRect().left){
-            console.log('equal  !!')
-            // this.setState({
-            //     player1Health: this.state.player1Health - 10
-            // })
-            // console.log(this.state.player1Health + "health")
-            // this.checkHealth()
+        let player1Pos = ReactDOM.findDOMNode(this.player1.current).childNodes[0].getBoundingClientRect().left
+        let player2Pos = ReactDOM.findDOMNode(this.player2.current).childNodes[0].getBoundingClientRect().left
+
+        if(player1Pos >= player2Pos ){
+            console.log('collision happend  !!')
             return true
 
-        } else {
+        }else {
             console.log('no collision  !!')
             return false
-
         }
-       
     }
-    checkHealth = () => {
+
+    calculateDistance = () => {
+        let distP1 = ReactDOM.findDOMNode(this.player1.current).childNodes[0].getBoundingClientRect()
+        let distP2 = ReactDOM.findDOMNode(this.player2.current).childNodes[0].getBoundingClientRect()
+
+        let distP1x = distP1.left + distP1.width/2  
+        let distP1y = distP1.top + distP1.height/2
+
+        let distP2x = distP2.left + distP2.width/2  
+        let distP2y = distP2.top + distP2.height/2
+
+        let sqrtDist = Math.pow(distP1x - distP2x, 2) + Math.pow(distP1y - distP2y, 2)
+        let distance = Math.sqrt(sqrtDist)
+        this.setState({
+            playersDist : this.state.playersDist
+        })
+        console.log("dis " +distance)
+    }
+
+    healthPlayer1 = () => {
+        if( this.state.player1Health === 0) {
+           alert("p1 is dead " + this.state.player1Health + " p2 is the winner !!") 
+           return true ;
+        }else {
+            this.setState({
+                player1Health: this.state.player1Health - 10
+            })
+        }
+    }
+
+    healthPlayer2 = () => {
         if( this.state.player2Health === 0) {
-            console.log("p2 is dead " + this.state.player2Health)
+           alert("p2 is dead " + this.state.player2Health + " p1 is the winner !!")
+           return true ;
         }else {
             this.setState({
                 player2Health: this.state.player2Health - 10
             })
-            console.log(this.state.player2Health + "health")
         }
-        
     }
 
-    //componentDidUpdate
     componentDidMount() {
-        this.checkCollision()
-        // this.checkHealth()
+        this.checkCollisionP1()
     }
+
     render () {  
         
         return (
-            <div>
-                <h1 onClick={this.handleClick}>The Start</h1> 
-                 <main>
-                    <canvas ref={this.canvas}
-                        width={ this.state.screen.width * this.state.screen.ratio }
-                        height={ this.state.screen.height * this.state.screen.ratio } />
-                    <Ken ref={this.player1} checkCollision= {this.checkCollision} />
-                    <Ryu ref={this.player2} checkCollision= {this.checkCollision} />
-                    {/* <selectCharacter /> */}
-                </main>
-            </div>
+            <main id="GameView">
+                    <aside className="split leftSide">
+                    <div className="healthBarP1">
+                        <div className="progressP1" style={{width: this.state.player1Health}}>
+                            {/* {this.state.player1Health +" health p1"} */}
+                        </div>
+                    </div>
+                    </aside>
+
+                    <aside className="split rightSide">
+                    <div className="healthBarP2">
+                        <div className="progressP2" style={{width: this.state.player2Health}}>
+                            {/* {this.state.player2Health +" health p2"} */}
+                        </div>
+                    </div>
+                    </aside>
+                    
+                <canvas id="GameCanvas" ref={this.canvas}
+                    width={ this.state.screen.width * this.state.screen.ratio }
+                    height={ this.state.screen.height * this.state.screen.ratio } /> 
+
+                <Ken ref={this.player1} 
+                collisionP1= {this.checkCollisionP1} 
+                distance ={this.calculateDistance}
+                healthP2 = {this.healthPlayer2} />
+                {/* <p>{this.state.player1Health +" health p1"}</p> */}
+
+                <Dudley ref={this.player2} 
+                collisionP1= {this.checkCollisionP1}
+                distance ={this.calculateDistance}
+                healthP1 = {this.healthPlayer1} />
+                {/* <p>{this.state.player2Health +" health p2"}</p> */}
+                {/* <selectCharacter /> */}
+            </main>
         )
     }
 }
   
 export default GameView;
+// if (this.state.player1Win === 2 ){
+//     alert("player 1 win ")
+// }else {
+//     this.setState({
+//         player1Win: this.state.player2Health + 1 
+//     })
+// }
--- src/game/Ken.js
+++ src/game/Ken.js
@@ -7,73 +7,101 @@ class Ken extends Component {
         super(props)
         this.Character = React.createRef()
     }
-    state ={
-        kenHealth: 50
-    }
     
     kenAttack = (event) => {
-        if(event.keyCode === 39){// 39 is the right arrow on the keyboard
-           if (!this.props.checkCollision()) {
+        if(event.keyCode === 68){// 68 is the letter D on the keyboard
+
+           if (!this.props.collisionP1()) {
+            //    this.props.distance()
             this.Character.current.classList.add('walk')
-            const originalMargin = this.Character.current.style.marginLeft || "0"
-            const originalMarginPx = parseInt(originalMargin.replace('px', '')) 
-            this.Character.current.style.marginLeft = originalMarginPx + 15 + "px";
+            let originalMargin = this.Character.current.style.marginLeft || "0"
+            let originalMarginPx = parseInt(originalMargin.replace('px', '')) 
+            this.Character.current.style.marginLeft =  originalMarginPx + 15 + "px";
 
            } else {
                console.log("no moving")
            }
 
-        }else if(event.keyCode === 37){// 37 is the left arrow on the keyboard
+        }else if(event.keyCode === 65){// 65 is the letter A on the keyboard
             this.Character.current.classList.add('walk')
             const originalMargin = this.Character.current.style.marginLeft || "0"
             const originalMarginPx = parseInt(originalMargin.replace('px', '')) 
             this.Character.current.style.marginLeft = originalMarginPx - 15 + "px";
 
-            // this.Character.current.animate([{transform: 'translateX(0px)'}, {transform: "translateX(50px)"}], {duration: 1000})
-            // setTimeout( () => { this.Character.current.classList.remove('walk'); }, 150);
-
         }else if (event.keyCode === 80) { // 80 is the letter P on the keyboard
-            
-            this.Character.current.classList.add('punch');
-            setTimeout( () => { this.Character.current.classList.remove('punch'); }, 150);
-            console.log("heeey you're punch ")
 
-        }else  if (event.keyCode === 66) { // 66 is the letter B on the keyboard
-            this.Character.current.classList.add('kick');
-            setTimeout( () => { this.Character.current.classList.remove('kick'); }, 500);
-            console.log("heeey you're kick")
+            if (this.props.distance() <= 50 ){
+                this.Character.current.classList.add('punch');
+                setTimeout( () => { this.Character.current.classList.remove('punch'); }, 150);
+                this.props.healthP2()
+                console.log("heeey you're punch ")
+            }else{
+                console.log('no kick')
+            }
+
+        }else if (event.keyCode === 66) { // 66 is the letter B on the keyboard
+
+            if (this.props.distance() <= 65 ){
+                this.Character.current.classList.add('kick');
+                setTimeout( () => { this.Character.current.classList.remove('kick'); }, 500);
+                this.props.healthP2()
+                console.log("heeey you're kick")
+
+            }else{
+                console.log('no kick')
+            }
 
         }else  if (event.keyCode === 67 ) { // 67 is the letter C on the keyboard
-            this.Character.current.classList.add('reversekick');
-            setTimeout( () => { this.Character.current.classList.remove('reversekick'); }, 500);
-            console.log("heeey you're reversekick")
+
+            if (this.props.distance() <= 65 ){
+                this.Character.current.classList.add('reversekick');
+                setTimeout( () => { this.Character.current.classList.remove('reversekick'); }, 500);
+                this.props.healthP2()
+                console.log("heeey you're reversekick")
+            }else{
+                console.log('no kick')
+            }
 
         }else  if (event.keyCode === 69 ) { // 69 is the letter E on the keyboard
-            this.Character.current.classList.add('tatsumaki');
-            setTimeout( () => { this.Character.current.classList.add('down'); }, 500)
-            setTimeout( () => { this.Character.current.classList.remove('tatsumaki'); }, 2000)
-            setTimeout( () => { this.Character.current.classList.remove('down'); }, 2000)
-            console.log("heeey you're tatsumaki")
+
+            if (this.props.distance() <= 65 ){
+                this.Character.current.classList.add('tatsumaki');
+                setTimeout( () => { this.Character.current.classList.add('down'); }, 500)
+                setTimeout( () => { this.Character.current.classList.remove('tatsumaki'); }, 2000)
+                setTimeout( () => { this.Character.current.classList.remove('down'); }, 2000)
+                this.props.healthP2()
+                console.log("heeey you're tatsumaki")
+            }else{
+                console.log('no kick')
+            }
 
         }else  if (event.keyCode === 71 ) { // 71 is the letter G on the keyboard
-            this.Character.current.classList.add('shoryuken');
-            setTimeout( () => { this.Character.current.classList.add('down'); }, 500)
-            setTimeout( () => { this.Character.current.classList.remove('shoryuken'); }, 1000)
-            setTimeout( () => { this.Character.current.classList.remove('down'); }, 1000)
-            console.log("heeey you're shoryuken")
+            
+            if (this.props.distance() <= 65 ){
+                this.Character.current.classList.add('shoryuken');
+                setTimeout( () => { this.Character.current.classList.add('down'); }, 500)
+                setTimeout( () => { this.Character.current.classList.remove('shoryuken'); }, 1000)
+                setTimeout( () => { this.Character.current.classList.remove('down'); }, 1000)
+                this.props.healthP2()
+                console.log("heeey you're shoryuken")
+            }else{
+                console.log('no kick')
+            }
 
-        }else  if (event.keyCode === 72 ) { // 72 is the letter H on the keyboard
+        }else  if (event.keyCode === 87) { // 72 is the letter W on the keyboard
+
             this.Character.current.classList.add('jump');
             setTimeout( () => { this.Character.current.classList.add('down'); }, 500)
             setTimeout( () => { this.Character.current.classList.remove('jump'); }, 1000)
             setTimeout( () => { this.Character.current.classList.remove('down'); }, 1000)
             console.log("heeey you're jump")
-        }else if(event.keyCode === 73){// 73 is the letter I on the keyboard
-            this.Character.current.classList.add('down');
-            setTimeout( () => { this.Character.current.classList.remove('down'); }, 250);
-            console.log("heeey you're down")
-
+           
         }
+        // else if(event.keyCode === 83){// 73 is the letter S on the keyboard
+        //     this.Character.current.classList.add('down');
+        //     setTimeout( () => { this.Character.current.classList.remove('down'); }, 250);
+        //     console.log("heeey you're down")
+        // }
     }
 
     componentDidMount(){
@@ -85,7 +113,7 @@ class Ken extends Component {
     // }
     render() {
         return (
-            <div>
+            <div className= "kenContainer">
                 <section className="ken" ref={this.Character} onKeyDown={ (e) =>  this.kenAttack(e) } >
                 </section>
             </div>
@@ -94,10 +122,7 @@ class Ken extends Component {
 }
 
 export default Ken ;
-// if(this.state.kenHealth == 0 ){
-//     console.log("you're die !!")
-// }else {
-//     this.setState({
-//         kenHealth: this.state.kenHealth - 10
-//     })
-// }
+
+
+// this.Character.current.animate([{transform: 'translateX(0px)'}, {transform: "translateX(50px)"}], {duration: 1000})
+// setTimeout( () => { this.Character.current.classList.remove('walk'); }, 150);
