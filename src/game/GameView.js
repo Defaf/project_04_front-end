6696817d00ca4121a84144eb65d6af2187a4a0f8
Defaf <defaf_kamfar@hotmail.com>
2019-08-01 00:24:32 +0300

    import the sound effect for K.O. and modify some condions

--- src/game/GameView.js
+++ src/game/GameView.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom'
 import './GameView.css'
 import Ken from './Ken'
 import Dudley from './Dudley'
+import KO from '../images/game/K.O.png'
+import ko_SE from '../sound_effect/KO_SE.mp3'
+import koFinish from '../images/game/KO_finish.png'
 
-let width = 1100
+let width = 900
 let height = 540
 const ratio = window.devicePixelRatio || 1;
 // window.innerHeight;
@@ -17,6 +20,8 @@ class GameView extends Component {
         this.player1 = React.createRef()
         this.player2 = React.createRef()
         this.progress = React.createRef()
+        this.playKO = this.playKO.bind(this);
+        this.soundKO = new Audio(ko_SE);
     }
 
     state = {
@@ -25,11 +30,13 @@ class GameView extends Component {
             height: height,
             ratio: ratio
         },
+        flag : false , 
         playersDist : 300,
         player1Health : 300,
         player2Health : 300,
         player1Win : 0, 
-        player2Win: 0 
+        player2Win: 0 ,
+        koImage: false
     };
     
     checkCollisionP1 = () => {
@@ -70,8 +77,11 @@ class GameView extends Component {
 
     healthPlayer1 = () => {
         if( this.state.player1Health === 0) {
-           alert("p1 is dead " + this.state.player1Health + " p2 is the winner !!") 
+        //    alert("p1 is dead " + this.state.player1Health + " p2 is the winner !!") 
+           this.playKO()
+           this.setState({ koImage: true })
            return true ;
+
         }else {
             this.setState({
                 player1Health: this.state.player1Health - 10
@@ -81,13 +91,20 @@ class GameView extends Component {
 
     healthPlayer2 = () => {
         if( this.state.player2Health === 0) {
-           alert("p2 is dead " + this.state.player2Health + " p1 is the winner !!")
+        //    alert("p2 is dead " + this.state.player2Health + " p1 is the winner !!")
+           this.playKO()
+           this.setState({ koImage: true })
            return true ;
+
         }else {
             this.setState({
                 player2Health: this.state.player2Health - 10
             })
         }
+    }
+
+    playKO = () => {
+        this.soundKO.play();
     }
 
     componentDidMount() {
@@ -98,21 +115,36 @@ class GameView extends Component {
         
         return (
             <main id="GameView">
+                
+                <div className="healthContainer">
+
                     <aside className="split leftSide">
-                    <div className="healthBarP1">
-                        <div className="progressP1" style={{width: this.state.player1Health}}>
-                            {/* {this.state.player1Health +" health p1"} */}
+                        <div className="healthBar healthBarP1">
+                            <div className="progressP1" style={{width: this.state.player1Health}}>
+                            </div>
                         </div>
-                    </div>
+                        <p className="playersName nameP1">RYU</p>
                     </aside>
 
                     <aside className="split rightSide">
-                    <div className="healthBarP2">
-                        <div className="progressP2" style={{width: this.state.player2Health}}>
-                            {/* {this.state.player2Health +" health p2"} */}
+                        <div className="healthBar healthBarP2">
+                            <div className="progressP2" style={{width: this.state.player2Health}}>
+                            </div>
                         </div>
-                    </div>
+                        <p className="playersName nameP2">DUDLEY</p>
                     </aside>
+
+                </div>
+                    
+
+                    <aside className="containerKO">
+                        <img src={KO} alt="K.O." className="KO"/> 
+                    </aside>
+
+                    <div class="image-container">
+                        {this.state.koImage && <img src={koFinish} alt="K.O.Finish"/>}
+                    </div>
+
                     
                  <canvas id="GameCanvas" ref={this.canvas}
                     width={ this.state.screen.width * this.state.screen.ratio }
@@ -121,16 +153,17 @@ class GameView extends Component {
                 <Ken ref={this.player1} 
                 collisionP1= {this.checkCollisionP1} 
                 distance ={this.calculateDistance}
-                healthP1 = {this.healthPlayer1}
-                healthP2 = {this.healthPlayer2}/>
+                // healthP1 = {this.healthPlayer1}
+                healthP2 = {this.healthPlayer2} />
                 {/* <p>{this.state.player1Health +" health p1"}</p> */}
 
                 <Dudley ref={this.player2} 
                 collisionP1= {this.checkCollisionP1}
                 distance ={this.calculateDistance}
-                healthP2 = {this.healthPlayer2}/>
+                // healthP2 = {this.healthPlayer2}
+                healthP1 = {this.healthPlayer1}
+                 />
                 {/* <p>{this.state.player2Health +" health p2"}</p> */}
-                {/* <selectCharacter /> */}
             </main>
         )
     }
